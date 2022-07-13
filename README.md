5c466d36119e78a39ac30face0e26c360416caf1
okba allaoua <o.allaoua@esi-sba.com>
2022-07-13 23:34:17 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bootstrap Learning Landing Page Using CSS Bootstrap Only Feauturing Dark and Light Mode
+
+
+# Dark Mode:
+![screenshot-dark](https://user-images.githubusercontent.com/96151694/178842248-1a29f1fa-4c63-4f1b-be64-745d79bd61db.png)
+
+<hr />
+
+# Light Mode:
+![screenshot-white](https://user-images.githubusercontent.com/96151694/178842396-5f9af47c-722f-462b-88a7-4333c14b93ba.png)
+
+"Learn Bootstrap" Landing Page Using CSS Bootstrap JS And HTML and it's feauturing dark and light mode.
+
+NOTE : THE WEBSITE IS HOSTED ON NETLIFY YOU CAN CHECK IT OUT : https://bootstraplearn.netlify.app/
+
+NOTE : The repository privacy, content can be changed any time.
---
+++ script/script.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector(".dark-mode"),
+    body = document.querySelector("body"),
+    navbar = document.querySelector(".navbar"),
+    hr = document.querySelectorAll("hr"),
+    footer_links = document.querySelectorAll(".footer-link"),
+    cards = document.querySelectorAll(".card"),
+    nav_link = document.querySelectorAll(".nav-link"),
+    nav_brand = document.querySelector(".navbar-brand"),
+    toggler = document.querySelector(".navbar-toggler");
+
+btn.addEventListener("click", function () {
+    if (body.classList.contains("bg-dark")) {
+        body.classList = "bg-white text-dark";
+        navbar.classList = "navbar navbar-dark bg-white navbar-expand-md position-sticky top-0";
+        hr.forEach(elem => elem.classList = "bg-dark");
+        cards.forEach(card => card.classList = "col card bg-dark text-white p-2 m-md-3 my-2 col-md-5");
+        nav_link.forEach(link => link.classList = "nav-link text-dark");
+        nav_brand.classList = "navbar-brand text-dark";
+        btn.textContent = "Dark";
+    } else {
+        body.classList = "bg-dark text-white";
+        navbar.classList = "navbar navbar-dark bg-dark navbar-expand-md position-sticky top-0";
+        hr.forEach(elem => elem.classList = "bg-white");
+        cards.forEach(card => card.classList = "col card bg-white text-dark p-2 m-md-3 my-2 col-md-5");
+        nav_link.forEach(link => link.classList = "nav-link text-white");
+        nav_brand.classList = "navbar-brand text-white";
+        btn.textContent = "Light";
+    }
+    btn.classList = "btn dark-mode btn-primary nav-link btn-md py-0 px-2 ms-md-2 mt-2 mt-md-0";
+    footer_links.forEach(footer_link => footer_link.classList = "nav-link text-primary");
+})
